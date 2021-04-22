9fbed04c92224279ca1b88a0544e02a34def613c
VaibhavManikpure <82816566+VaibhavManikpure@users.noreply.github.com>
2021-04-22 17:24:47 +0530

    by vaibhav 2 changes
    added m2 method

--- GITDemo01/src/Demo1.java
+++ GITDemo01/src/Demo1.java
@@ -1,9 +1,15 @@
 
 public class Demo1 {
 	public static void main(String[] args) {
+		Demo1 d=new Demo1();
+		d=m01;
+		d=m02;
 		System.out.println("hello demo");
 	}
 public void m01() {
 	System.out.println("01");
 }
+public void m02() {
+	System.out.println("02");
+}	
 }

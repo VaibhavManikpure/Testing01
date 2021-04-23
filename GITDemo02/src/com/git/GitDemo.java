2b41de612160e12607fd13f37561a3740d6cb7d2
Gaurav PC <Gaurav PC@VAIBHAV>
2021-04-23 19:39:31 +0530

    by vaibhav2

---
+++ GITDemo02/src/com/git/GitDemo.java
@@ -0,0 +1,17 @@
+package com.git;
+
+public class GitDemo {
+
+	public static void main(String[] args) {
+		GitDemo d=new GitDemo();
+		d.m01();
+		d.m02();
+		System.out.println("hello demo");
+	}
+	public void m01() {
+		System.out.println("01");
+	}
+	public void m02() {
+		System.out.println("02");
+	}	
+}
